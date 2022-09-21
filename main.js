6b56a0a1796c28488a0d8ddf6f985f8362bd7886
crushcoward <2408212858@qq.com>
2022-09-21 11:40:56 +0800

    modify main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'image/firefox.jpg') {
-        myImage.setAttribute('src', 'image/firefox2.png');
+    if(mySrc === 'firefox.jpg') {
+        myImage.setAttribute('src', 'firefox2.png');
     } else {
-        myImage.setAttribute('src', 'image/firefox.jpg');
+        myImage.setAttribute('src', 'firefox.jpg');
     }
 }
 let myButton = document.querySelector('button');
